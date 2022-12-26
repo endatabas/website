6683b11896ee7e748d7b556a84dbbe67385ac1f2
Steven Deobald <steven@deobald.ca>
2022-12-26 03:55:16 -0600

    Some CSS tweaks: bib has theme now, fixing padding on quotes

--- bin/build.sh
+++ bin/build.sh
@@ -5,4 +5,8 @@ set -e
 (
     cd $(dirname "$0")/../docs/bib/
     bibtex2html bibliography.bib
+    # MacOS uses BSD sed, which requires a param for -i, so...
+    sed 's/<title>bibliography<\/title>/<title>bibliography<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib.css" \/>/g' bibliography.html > bibliography_themed.html
+    mv bibliography.html bibliography.html.bak
+    mv bibliography_themed.html bibliography.html
 )
