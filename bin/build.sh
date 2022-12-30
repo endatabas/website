28cfe5fec77bc6fe7c2afd45977168c05e0af85c
Steven Deobald <steven@deobald.ca>
2022-12-30 17:00:44 -0600

    Backwards "tokenizing" of the bibliography page
    * pushing this, despite its silliness. I'll just do proper string
      templates instead rather than dragging sed this far.

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
-set -x
-set -e
+#set -x
+#set -e
+
+HEADER=`cat $(dirname "$0")/../docs/partials/_header.html`
+HEADER_FILE=`realpath $(dirname "$0")/../docs/partials/_header.html`
+
 (
+    printf "\nGenerating bibliography...\n"
     cd $(dirname "$0")/../docs/bib/
     bibtex2html bibliography.bib
     # MacOS uses BSD sed, which requires a param for -i, so...
-    sed 's/<title>bibliography<\/title>/<title>bibliography<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib.css" \/>/g' bibliography.html > bibliography_themed.html
+    sed 's/<title>bibliography<\/title>/<title>bibliography<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/beta2.css" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib.css" \/>/g' bibliography.html > bibliography_themed.html
     mv bibliography.html bibliography.html.bak
     mv bibliography_themed.html bibliography.html
+)
+
+printf "\nReplacing bib tokens...\n"
+(
+    # "s/<body>/<body color-mode=\"user\">$HEADER/g"
+    cd $(dirname "$0")/../docs/bib/
+    echo $HEADER_FILE
+    sed "/<body>/{
+        s/<body>/<body color-mode=\"user\">/g
+        r $HEADER_FILE
+    }" bibliography.html > bibliography_tokenized.html
+    mv bibliography.html bibliography.html.bak2
+    mv bibliography_tokenized.html bibliography.html
 )
