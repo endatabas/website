07fb93ee8ffddedbe8022f5eb414a0776b668b27
Steven Deobald <steven@deobald.ca>
2022-12-31 14:49:29 -0600

    Use bibtex default sort order (author last name)

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@
     printf "\nGenerating bibliography...\n"
     cd $(dirname "$0")/../src/bib/
     # -q is too quiet, -w doesn't actually stop on warning:
-    bibtex2html -noabstract -nokeywords bibliography.bib
+    bibtex2html -a -noabstract -nokeywords bibliography.bib
     perl -p0e 's/.*<table>/<table>/s' bibliography.html > ../../src/partials/_bib_table.html.tmp
     perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/_bib_table.html.tmp > ../../src/partials/_bib_table.html
     perl -p0e 's/<title>bibliography.bib<\/title>/<title>bibliography.bib<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib_bib.css" \/>/g' bibliography_bib.html > ../../src/bibliography_bib.html
