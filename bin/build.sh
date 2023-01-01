552d9117980f3497f048a2768baad03c8a75c343
Steven Deobald <steven@deobald.ca>
2022-12-31 19:42:11 -0600

    Using bib2bib to add "References"
    * references + research = bibliography
    * references will contain papers, articles, etc that the code could
      link to directly in a comment, as a "citation"
    * src/bib/bibliography.bib is now a target (thus ignored)

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,13 @@
 (
     printf "\nGenerating bibliography...\n"
     cd $(dirname "$0")/../src/bib/
+    bib2bib -ob bibliography.bib references.bib research.bib
     # -q is too quiet, -w doesn't actually stop on warning:
+    bibtex2html -a -noabstract -nokeywords references.bib
+    perl -p0e 's/.*<table>/<table>/s' references.html > ../../src/partials/_ref_table.html.tmp
+    perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/_ref_table.html.tmp > ../../src/partials/_ref_table.html
+    perl -p0e 's/<title>references.bib<\/title>/<title>references.bib<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib_bib.css" \/>/g' references_bib.html > ../../src/references_bib.html
+
     bibtex2html -a -noabstract -nokeywords bibliography.bib
     perl -p0e 's/.*<table>/<table>/s' bibliography.html > ../../src/partials/_bib_table.html.tmp
     perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/_bib_table.html.tmp > ../../src/partials/_bib_table.html
@@ -16,6 +22,8 @@
     printf "\nM4 Replacing...\n"
     cd $(dirname "$0")/../
     m4 -I "./src/partials/" src/beta2.html > docs/beta2.html
+    m4 -I "./src/partials/" src/references.html > docs/references.html
+    m4 -I "./src/partials/" src/references_bib.html > docs/references_bib.html
     m4 -I "./src/partials/" src/bibliography.html > docs/bibliography.html
     m4 -I "./src/partials/" src/bibliography_bib.html > docs/bibliography_bib.html
 )
