f1da9e313ad87ff66ec259e8833d43ac59858f67
Steven Deobald <steven@deobald.ca>
2022-12-31 20:02:17 -0600

    Extract some fns for the build script perl violence

--- bin/build.sh
+++ bin/build.sh
@@ -3,19 +3,26 @@
 #set -x
 #set -e
 (
+    strip_table () {
+        perl -p0e 's/.*<table>/<table>/s' $1 > ../../src/partials/$2.tmp
+        perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/$2.tmp > ../../src/partials/$2
+    }
+
+    style_bib_bib () {
+        perl -p0e "s/<title>$1.bib<\/title>/<title>$1.bib<\/title>\n<link rel=\"stylesheet\" href=\"\/resources\/css\/dark.css\" media=\"(prefers-color-scheme: dark)\" \/>\n<link rel=\"stylesheet\" href=\"\/resources\/css\/light.css\" media=\"(prefers-color-scheme: light)\" \/>\n<link rel=\"stylesheet\" href=\"\/resources\/css\/bib_bib.css\" \/>/g" $1_bib.html > ../../src/$1_bib.html
+    }
+
     printf "\nGenerating bibliography...\n"
     cd $(dirname "$0")/../src/bib/
     bib2bib -ob bibliography.bib references.bib research.bib
     # -q is too quiet, -w doesn't actually stop on warning:
     bibtex2html -a -noabstract -nokeywords references.bib
-    perl -p0e 's/.*<table>/<table>/s' references.html > ../../src/partials/_ref_table.html.tmp
-    perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/_ref_table.html.tmp > ../../src/partials/_ref_table.html
-    perl -p0e 's/<title>references.bib<\/title>/<title>references.bib<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib_bib.css" \/>/g' references_bib.html > ../../src/references_bib.html
+    strip_table "references.html" "_ref_table.html"
+    style_bib_bib "references"
 
     bibtex2html -a -noabstract -nokeywords bibliography.bib
-    perl -p0e 's/.*<table>/<table>/s' bibliography.html > ../../src/partials/_bib_table.html.tmp
-    perl -p0e 's/<\/table>.*/<\/table>\n/s' ../../src/partials/_bib_table.html.tmp > ../../src/partials/_bib_table.html
-    perl -p0e 's/<title>bibliography.bib<\/title>/<title>bibliography.bib<\/title>\n<link rel="stylesheet" href="\/resources\/css\/dark.css" media="(prefers-color-scheme: dark)" \/>\n<link rel="stylesheet" href="\/resources\/css\/light.css" media="(prefers-color-scheme: light)" \/>\n<link rel="stylesheet" href="\/resources\/css\/bib_bib.css" \/>/g' bibliography_bib.html > ../../src/bibliography_bib.html
+    strip_table "bibliography.html" "_bib_table.html"
+    style_bib_bib "bibliography"
 )
 
 (
